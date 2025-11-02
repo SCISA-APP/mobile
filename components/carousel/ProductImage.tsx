6921e58774214c9ae00bfee1e0e078e64755eba9
Gyamfi Agyemang <gyamfiagyemang999@gmail.com>
2025-11-02 19:28:03 +0100

    pcreated a file for product Image

---
+++ components/carousel/ProductImage.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ProductImage = () => {
+  return (
+    <View>
+      <Text>ProductImage</Text>
+    </View>
+  )
+}
+
+export default ProductImage
+
+const styles = StyleSheet.create({})
